85312506b639e42e923e7fb32e5d4f5f4074aa48
Nikita Kravchenko <nickravchenko98@gmail.com>
2024-11-10 23:49:12 -0400

    Initial commit

---
+++ lib/Screen/HomePage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: const Text('Home Page'),
+    );
+  }
+}
---
+++ lib/widgets/constant.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const Color kYellowLight = Color(0xFFFFF9EC);
+const Color kYellowDark = Color.fromARGB(255, 121, 89, 0);
+const Color kYellow = Color(0xFFFFD460);
+
+const Color kBlueLight = Color(0xFFE6F1FF);
+const Color kBlueDark = Color.fromRGBO(0, 50, 177, 1);
+const Color kBlue = Color(0xFF4A7CFF);
+
+const Color kRedLight = Color.fromARGB(255, 255, 0, 0);
+const Color kRedDark = Color(0xFF4CFFB2);
+const Color kRed = Color(0xFF4CFFB2);
+
+const Color kwhite = Colors.white;
+const Color kdark = Colors.grey;
+
+class MyThemeColor {
+  static const Color backgroundColor = Colors.white;
+  static const Color textColor = Colors.black;
+
+  static const Color subText = Color(0xffc6c6c6);
+}
